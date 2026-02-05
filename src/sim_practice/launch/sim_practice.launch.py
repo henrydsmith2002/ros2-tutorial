90cef64b97c9c3f49ea83e6b135dfd0da8f55a52
Henry Smith <hsmith40@byu.edu>
2026-02-05 10:05:05 -0700

    first working urdf pieces

---
+++ src/sim_practice/launch/sim_practice.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory("sim_practice")
+    urdf_path = os.path.join(pkg_share, 'urdf','sim_practice.urdf.xml')
+
+    with open(urdf_path, 'r') as f:
+        robot_description = f.read()
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}],
+            output='screen'
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen'
+        ),
+    ])
